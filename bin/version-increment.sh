97983593a46e5096bfff2d7990e62ef41b7b2639
Volodymyr Shvaiko <cub4lt@Mgmail.com>
2019-06-12 16:29:38 +0300

    fix: fix git path

--- bin/version-increment.sh
+++ bin/version-increment.sh
@@ -4,5 +4,5 @@
 git config --global user.email "ephordnb@gmail.com"
 git config --global user.name "ephor"
 standard-version -m "chore(release): publish %s [ci skip]"
-git remote set-url origin https://$GITHUB_TOKEN@github.com/edenlabllc/mithril.web.git
+git remote set-url origin https://$GITHUB_TOKEN@github.com/edenlabllc/man.web.git
 git push --follow-tags origin master
